bf5761c84cfec21ffc84f62540e5c1ebd5844723
imanshgupta <iamtheanshgupta@gmail.com>
2021-04-15 13:54:54 +0530

    arithmeric operations

---
+++ arithmitic.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "entr a number"
+read a
+echo "enter a number"
+read b
+echo "addition values `expr $a + $b`"
+echo "addition values `expr $a + $b`"
+echo "minus values `expr $a - $b`"
+echo "multiplied by values `expr $a \* $b`"
+echo "devided by values `expr $a / $b`"
+echo "remainder values `expr $a % $b`"
+echo "addition values `expr $a + $b`"
+
+echo "Completed Sucessfully"
